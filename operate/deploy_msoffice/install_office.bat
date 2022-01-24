5130d8e156c37869e3e5e22914ca95b3d5167aa3
szpeter80 <szpeter80@github.com>
2022-01-24 16:42:19 +0100

    ~ deploy_msoffice: clarification

--- operate/deploy_msoffice/install_office.bat
+++ operate/deploy_msoffice/install_office.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
-ECHO "You need Office Deployment tools: https://www.microsoft.com/en-us/download/details.aspx?id=49117"
+ECHO "You need Office Deployment tools (setup.exe): https://www.microsoft.com/en-us/download/details.aspx?id=49117"
 ECHO "Direct link to ODT install: https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117"
-ECHO "Please update your license keys in the XML file !"
-ECHO setup.exe /download /configure office219_only.xml
+ECHO "Please update your license keys in the XML file before running the installer!"
+ECHO Install Office: setup.exe /configure office219_only.xml
+ECHO Create install material only: setup.exe /download office219_only.xml
