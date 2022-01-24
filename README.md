e092cda01086b3d76980cc00b3368724714c8bfe
szpeter80 <szpeter80@github.com>
2022-01-24 14:56:58 +0100

    +Office deployment tool

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # About
 
 This repository contains various small items, which are shared with the intention that somebody else might find it useful.
+
+
+## Cheatsheet
+
+C# and Python quick reference
+
+## Operate
+
+Small scripts related to install and operate a system
---
+++ operate/deploy_msoffice/install_office.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+ECHO "You need Office Deployment tools: https://www.microsoft.com/en-us/download/details.aspx?id=49117"
+ECHO "Direct link to ODT install: https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117"
+ECHO "Please update your license keys in the XML file !"
+ECHO setup.exe /download /configure office219_only.xml
