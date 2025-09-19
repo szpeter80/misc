e30a9ca19cd8d70117c61410059960e054beaa9b
Peter <szpeter80@users.noreply.github.com>
2025-09-19 16:47:14 +0200

    ~ Update choco base packages

--- operate/choco/02_choco_install_base.bat
+++ operate/choco/02_choco_install_base.bat
@@ -1,2 +1,3 @@
 @ECHO OFF
-choco install  choco-cleaner adobereader notepadplusplus 7zip vlc sysinternals freecommander GoogleChrome-AllUsers libreoffice-fresh teamviewer anydesk
+choco install  choco-cleaner adobereader sumatrapdf notepadplusplus 7zip vlc sysinternals freecommander GoogleChrome-AllUsers libreoffice-fresh anydesk
+
